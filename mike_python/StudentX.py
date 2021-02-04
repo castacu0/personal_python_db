f1b1fb4ec09d542548e417229892a3c258eed651
Cesar Castanon A <emprendedorcesar0@gmail.com>
2021-02-03 22:04:56 -0600

    Adding a Python png picture

---
+++ mike_python/StudentX.py
@@ -0,0 +1,9 @@
+#This is the class keeper
+
+class Student:
+    
+    def __init__(self, name, faculty, gpa, approved):
+         self.name = name
+         self.faculty = faculty
+         self.gpa = gpa
+         self.approved = approved
---
+++ mike_python/objects.py
@@ -0,0 +1,6 @@
+#This is my main document where I'll be using my 'StudentX.py' file
+from StudentX import Student
+
+pilot_student = Student("Frodo","The Fellowship", 3.9, True)
+
+print(pilot_student.gpa)
