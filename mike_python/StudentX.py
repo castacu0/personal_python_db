b6d0c117d51670a6d571d3741252985f0ab6cb25
Cesar Castanon A <emprendedorcesar0@gmail.com>
2021-02-03 23:24:39 -0600

    Worked with Classes and Objects

--- mike_python/StudentX.py
+++ mike_python/StudentX.py
@@ -1,9 +1,16 @@
 #This is the class keeper
 
 class Student:
-    
-    def __init__(self, name, faculty, gpa, approved):
-         self.name = name
-         self.faculty = faculty
-         self.gpa = gpa
-         self.approved = approved
+    """This is my Student data type.
+    The values ending with _Xz are a way to identify them.
+    I will print the ones to the left
+    ---> self.name    NOT    name_XZ
+    print(Student.name)"""
+
+    def __init__(self, name_Xz, faculty_Xz, gpa_Xz, approved_Xz):
+         self.name = name_Xz
+         self.faculty = faculty_Xz
+         self.gpa = gpa_Xz
+         self.approved = approved_Xz
+
+print(help(Student))
--- mike_python/objects.py
+++ mike_python/objects.py
@@ -1,6 +1,6 @@
 #This is my main document where I'll be using my 'StudentX.py' file
 from StudentX import Student
 
-pilot_student = Student("Frodo","The Fellowship", 3.9, True)
+student_1 = Student("Frodo","The Fellowship", 3.9, True)
 
-print(pilot_student.gpa)
+print(student_1.gpa)
