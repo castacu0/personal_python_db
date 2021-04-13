94930cf2da19691083a0d732e611bbf09738eb9f
Cesar Castanon A <emprendedorcesar0@gmail.com>
2021-04-13 10:28:41 -0500

    Class 8 and 9 finished

---
+++ intermediate_python_pl/lists_and_dicts.py
@@ -0,0 +1,30 @@
+def main():
+    my_list = [False, "Random", 5.8, 3]
+    my_dicts = {"firstname":"Robert", "lastname":"Acuna",}
+
+    super_list = [
+        {"firstname":"Robert", "lastname":"Acuna"},
+        {"firstname":"Juan", "lastname":"Mendez"},
+        {"firstname":"Lupe", "lastname":"Yao"},
+        {"firstname":"Marco", "lastname":"Lole"},
+    ] #This is a list of Dicts
+ 
+    super_dict = {
+        "natural_n" : [1, 2, 3, 4, 5],
+        "integer_n" : [-1, -2, 3, 4, -5],
+        "float_n" : [1.3, 2.9, 3.6, 4.8, 5.2],
+    } #This is a dict of Lists
+
+    for key1, value2 in super_dict.items():
+        print(f" --> {key1} : {value2}\n")
+
+#BOTH WORK
+    for i in super_list:
+        print(i["firstname"] , "-" , i["lastname"])  #"I was missing the commas"
+    
+    for i in super_list:
+        for key, values in i.items():
+            print(key,": ", values);
+
+if __name__ == '__main__':
+    main() #the caller / entry point
