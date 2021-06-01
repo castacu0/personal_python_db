1a43a1a82c13492c8bd6f3145de0152d38e0673b
Cesar Castanon A <emprendedorcesar0@gmail.com>
2021-05-31 21:36:27 -0500

    try, except, and finally now in my notes. All worked

--- w3s_course_web/quick_experiments.py
+++ w3s_course_web/quick_experiments.py
@@ -3,4 +3,20 @@
 # if not type(x) is int:
 #   raise TypeError("Only integers are allowed")
 
-print(dir(locals()['__builtins__']))
+#print(dir(locals()['__builtins__']))
+
+# import module sys to get the type of exception
+import sys
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
