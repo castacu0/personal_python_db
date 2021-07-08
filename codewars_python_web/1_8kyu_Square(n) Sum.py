8a3464b0da38c911d38e55d7f57c22d61d0e20bf
Cesar Castanon A <emprendedorcesar0@gmail.com>
2021-07-07 19:05:15 -0500

    py dir 2

--- codewars_python_web/1_8kyu_Square(n) Sum.py
+++ codewars_python_web/1_8kyu_Square(n) Sum.py
@@ -3,6 +3,7 @@ each number passed into it and then sums the results together.
 
 For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9
 
+#Good Practice
 numbers = [1,2,2]
 def square_sum(numbers):
     return sum(x**2 for x in numbers)
